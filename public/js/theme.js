2aab310ce452ae930e97f524266bf0b74612c32f
Nikita Malashko <nik.malashko@mail.ru>
2021-05-02 18:13:04 +0300

    dark/light theme with user preferences support

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,7 +1,18 @@
 const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 
-if (currentTheme) {
+if (!currentTheme || currentTheme == 'user-pref') {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        toggleSwitch.checked = true;
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        toggleSwitch.checked = false;
+    }  
+    localStorage.setItem('theme', 'user-pref'); 
+}
+else {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme == 'dark' ) {
         toggleSwitch.checked = true;
@@ -19,4 +30,19 @@ const switchTheme = (e) => {
     }
 }
 
+
+const getUserPref = (e) => {
+    if (e.matches) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        toggleSwitch.checked = true;
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        toggleSwitch.checked = false;
+    }   
+    localStorage.setItem('theme', 'user-pref');
+}
+
+
 toggleSwitch.addEventListener('change', switchTheme, false);
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', getUserPref, false)
